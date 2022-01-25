241b0af8ec758e8469a60d44bdd639978e2c7959
mrNorii <ksabiron03@gmail.com>
2022-01-25 13:37:07 +0100

    create some views and new file "urls"

--- Samochody/Modele/views.py
+++ Samochody/Modele/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Modele
+from rest_framework import viewsets
+from .serializer import ModeleSerializer
 
 # Create your views here.
 
 def index(request):
     ask = Modele.objects.all()
     return HttpResponse(ask)
+
+class ModeleViewSet(viewsets.ModelViewSet):
+    queryset = Modele.objects.all()
+    serializer_class = ModeleSerializer
+
