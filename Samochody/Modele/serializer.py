d26c7b18a5d5e48e6b93e66d7e3166e16247a5a7
mrNorii <ksabiron03@gmail.com>
2022-01-25 14:21:26 +0100

    Edit and Delete made by id, you need to add number of id in page link and then you can Edit or Delete object

--- Samochody/Modele/serializer.py
+++ Samochody/Modele/serializer.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class ModeleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Modele
-        fields = ['Marka', 'Model', 'Rok', 'Przebieg', 'Pojemnosc_Silnika', 'Rodzaj_Paliwa', 'Cena', 'Opis']
+        fields = ['id','Marka', 'Model', 'Rok', 'Przebieg', 'Pojemnosc_Silnika', 'Rodzaj_Paliwa', 'Cena', 'Opis']
