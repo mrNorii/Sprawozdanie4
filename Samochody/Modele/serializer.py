3f761918f25c045f011c9562bbf96c1137f77ea8
mrNorii <ksabiron03@gmail.com>
2022-01-25 13:27:33 +0100

    create class "Meta" with fields from "Modele" class

--- Samochody/Modele/serializer.py
+++ Samochody/Modele/serializer.py
@@ -1,3 +1,7 @@
 from .models import Modele
 from rest_framework import serializers
 
+class ModeleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Modele
+        fields = ['Id', 'Marka', 'Model', 'Rok', 'Przebieg', 'Pojemnosc_Silnika', 'Rodaj_Paliwa', 'Cena', 'Opis']
