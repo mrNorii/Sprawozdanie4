61013321b97924d35b66e49b9e7e610e2bd510eb
mrNorii <ksabiron03@gmail.com>
2022-01-25 12:32:45 +0100

    class "Modele" added

--- Samochody/Modele/models.py
+++ Samochody/Modele/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Modele(models.Model):
+    Marka = models.CharField(max_length=100)
+    Model = models.CharField(max_length=100)
+    Rok = models.IntegerField(max_length=7)
+    Przebieg = models.IntegerField(max_length=7)
+    Pojemnosc_Silnika = models.IntegerField(max_length=4)
+    Rodzaj_Paliwa = models.CharField(max_length=20)
+    Cena = models.DecimalField(max_digits=12, decimal_places=2)
+    Opis = models.TextField(blank=True)
