063cc1c5c570b2f0f932ddcb9d2fec8360a57b37
mrNorii <ksabiron03@gmail.com>
2022-01-28 00:43:48 +0100

    added images

--- Samochody/Modele/models.py
+++ Samochody/Modele/models.py
@@ -11,5 +11,6 @@ class Modele(models.Model):
     Rodzaj_Paliwa = models.CharField(max_length=20)
     Cena = models.DecimalField(max_digits=12, decimal_places=2)
     Opis = models.TextField(blank=True)
+    Zdjecie = models.ImageField(null=True, blank=True)
     def __str__(self):
         return self.Marka + " " + self.Model + " "
--- Samochody/Samochody/settings.py
+++ Samochody/Samochody/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,6 +60,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
+
+
 
 ROOT_URLCONF = 'Samochody.urls'
 
@@ -128,7 +131,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
